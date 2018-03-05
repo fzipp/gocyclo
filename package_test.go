ae9815a0a82ba0548cf22a110ef08302e23322bd
Gregory Vincic <gregoryv@axis.com>
2018-03-05 13:21:31 +0100

    Test Filter func

--- package_test.go
+++ package_test.go
@@ -1,10 +1,40 @@
 package gocyclo_test
 
 import (
+	"fmt"
 	"github.com/gregoryv/gocyclo"
+	"path/filepath"
 	"testing"
 )
 
 func TestAnalyze(t *testing.T) {
-	gocyclo.Analyze([]string{})
+	files := must(filepath.Glob("*.go"))
+	all := append(files, "cmd/")
+	result, ok := gocyclo.Assert(all, 5)
+	if !ok {
+		for _, l := range result {
+			fmt.Println(l)
+		}
+		t.Fail()
+	}
+	result, ok = gocyclo.Assert(all, -1)
+	if ok {
+		t.Error("Complexity should at least be 1")
+	}
+}
+
+func TestFilter(t *testing.T) {
+	files := must(filepath.Glob("*.go"))
+	result := gocyclo.Analyze(files)
+	filtered := gocyclo.Filter(result, 1, -1)
+	if len(filtered) != 1 {
+		t.Fail()
+	}
+}
+
+func must(result []string, err error) []string {
+	if err != nil {
+		panic(err)
+	}
+	return result
 }
