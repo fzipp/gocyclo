81a5c14840da3e1319a6effe8780fdf92e94d82a
Frederik Zipp <fzipp@gmx.de>
2020-10-18 17:43:19 +0200

    Add tests for SortAndFilter

--- stats_test.go
+++ stats_test.go
@@ -1,6 +1,7 @@
 package gocyclo_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/fzipp/gocyclo"
@@ -66,6 +67,81 @@ func TestTotalComplexity(t *testing.T) {
 		got := tt.stats.TotalComplexity()
 		if got != tt.want {
 			t.Errorf("Total complexity for %q, got: %d, want: %d", tt.stats, got, tt.want)
+		}
+	}
+}
+
+
+func TestSortAndFilter(t *testing.T) {
+	tests := []struct {
+		stats gocyclo.Stats
+		top int
+		over int
+		want  gocyclo.Stats
+	}{
+		{
+			stats: gocyclo.Stats{
+				{Complexity: 1},
+				{Complexity: 4},
+				{Complexity: 2},
+				{Complexity: 3},
+			},
+			top: -1, over: 0,
+			want: gocyclo.Stats{
+				{Complexity: 4},
+				{Complexity: 3},
+				{Complexity: 2},
+				{Complexity: 1},
+			},
+		},
+		{
+			stats: gocyclo.Stats{
+				{Complexity: 1},
+				{Complexity: 2},
+				{Complexity: 3},
+				{Complexity: 4},
+			},
+			top: 2, over: 0,
+			want: gocyclo.Stats{
+				{Complexity: 4},
+				{Complexity: 3},
+			},
+		},
+		{
+			stats: gocyclo.Stats{
+				{Complexity: 1},
+				{Complexity: 2},
+				{Complexity: 4},
+				{Complexity: 4},
+				{Complexity: 5},
+			},
+			top: -1, over: 3,
+			want: gocyclo.Stats{
+				{Complexity: 5},
+				{Complexity: 4},
+				{Complexity: 4},
+			},
+		},
+		{
+			stats: gocyclo.Stats{
+				{Complexity: 1},
+				{Complexity: 2},
+				{Complexity: 3},
+				{Complexity: 4},
+				{Complexity: 5},
+			},
+			top: 2, over: 2,
+			want: gocyclo.Stats{
+				{Complexity: 5},
+				{Complexity: 4},
+			},
+		},
+	}
+	for _, tt := range tests {
+		got := tt.stats.SortAndFilter(tt.top, tt.over)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("Sort and filter (top %d over %d) for %q, got: %q, want: %q",
+				tt.top, tt.over, tt.stats, got, tt.want)
 		}
 	}
 }
