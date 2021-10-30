44804ba326282759f6b21875e1e89840fb3aea25
fzipp <fzipp@gmx.de>
2021-10-30 10:51:11 +0200

    support method receivers with type parameters for Go 1.18

--- analyze.go
+++ analyze.go
@@ -138,15 +138,3 @@ func funcName(fn *ast.FuncDecl) string {
 	}
 	return fn.Name.Name
 }
-
-// recvString returns a string representation of recv of the
-// form "T", "*T", or "BADRECV" (if not a proper receiver type).
-func recvString(recv ast.Expr) string {
-	switch t := recv.(type) {
-	case *ast.Ident:
-		return t.Name
-	case *ast.StarExpr:
-		return "*" + recvString(t.X)
-	}
-	return "BADRECV"
-}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/fzipp/gocyclo
 
-go 1.16
+go 1.18
